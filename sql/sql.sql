f70c3d4e49107e2ad8b8ace887ba0104880f31e7
Victor Nevola <victornevolapn@gmail.com>
2022-01-09 19:30:50 -0300

    feat: create table for publicacoes

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,8 +1,9 @@
 CREATE DATABASE IF NOT EXISTS `devbook`;
 USE `devbook`;
 
-DROP TABLE IF EXISTS `usuarios`;
+DROP TABLE IF EXISTS `publicacoes`;
 DROP TABLE IF EXISTS `seguidores`;
+DROP TABLE IF EXISTS `usuarios`;
 
 CREATE TABLE `usuarios` (
   id int auto_increment primary key,
@@ -25,5 +26,20 @@ CREATE TABLE `seguidores` (
   ON DELETE CASCADE,
 
   primary key (usuario_id, seguidor_id)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `publicacoes` (
+  id int auto_increment primary key,
+  titulo varchar(100) NOT NULL,
+  conteudo varchar(300) NOT NULL,
+
+  autor_id int NOT NULL,
+  FOREIGN KEY (autor_id)
+  REFERENCES usuarios(id)
+  ON DELETE CASCADE,
+  
+  curtidas int default 0,
+  criadoEm timestamp default current_timestamp()
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
