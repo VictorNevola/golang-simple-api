c93ae3377a62a39324d9d2f47b3c7dcbd34f3d57
Victor Nevola <victornevolapn@gmail.com>
2022-01-13 22:26:39 -0300

    chore: add data on db

--- sql/data.sql
+++ sql/data.sql
@@ -1,11 +1,11 @@
 INSERT INTO usuarios (nome, nick, email, senha)
 values 
-('João', 'joao', 'joao@gmail.com', '$2a$10$ByMlQQpTT3LwwY8uNPT9pu1zENTtG/qcQyE3kj9h1eA0Qb4.IuYh2'),
-('Maria', 'maria', 'maria@gmail.com', '$2a$10$ByMlQQpTT3LwwY8uNPT9pu1zENTtG/qcQyE3kj9h1eA0Qb4.IuYh2'),
-('Pedro', 'pedro', 'pedro@gmail.com', '$2a$10$ByMlQQpTT3LwwY8uNPT9pu1zENTtG/qcQyE3kj9h1eA0Qb4.IuYh2'),
-('Ana', 'ana', 'ana@gmail.com', '$2a$10$ByMlQQpTT3LwwY8uNPT9pu1zENTtG/qcQyE3kj9h1eA0Qb4.IuYh2'),
-('José', 'jose', 'jose@gmail.com', '$2a$10$ByMlQQpTT3LwwY8uNPT9pu1zENTtG/qcQyE3kj9h1eA0Qb4.IuYh2'),
-('Joana', 'joana', 'joana@gmail.com', '$2a$10$ByMlQQpTT3LwwY8uNPT9pu1zENTtG/qcQyE3kj9h1eA0Qb4.IuYh2');
+('João', 'joao', 'joao@gmail.com', '$2a$10$Wkyoxk90Q9dQiBcoLnBRhOZvG1ecSV/u6MrhQhv1ZwSbtfduswrA.'),
+('Maria', 'maria', 'maria@gmail.com', '$2a$10$Wkyoxk90Q9dQiBcoLnBRhOZvG1ecSV/u6MrhQhv1ZwSbtfduswrA.'),
+('Pedro', 'pedro', 'pedro@gmail.com', '$2a$10$Wkyoxk90Q9dQiBcoLnBRhOZvG1ecSV/u6MrhQhv1ZwSbtfduswrA.'),
+('Ana', 'ana', 'ana@gmail.com', '$2a$10$Wkyoxk90Q9dQiBcoLnBRhOZvG1ecSV/u6MrhQhv1ZwSbtfduswrA.'),
+('José', 'jose', 'jose@gmail.com', '$2a$10$Wkyoxk90Q9dQiBcoLnBRhOZvG1ecSV/u6MrhQhv1ZwSbtfduswrA.'),
+('Joana', 'joana', 'joana@gmail.com', '$2a$10$Wkyoxk90Q9dQiBcoLnBRhOZvG1ecSV/u6MrhQhv1ZwSbtfduswrA.');
 
 INSERT into seguidores (usuario_id, seguidor_id)
 values
@@ -21,3 +21,16 @@ values
 (4, 1),
 (4, 2),
 (4, 3);
+
+INSERT into publicacoes (titulo, conteudo, autor_id)
+values
+("Publicacao do usuario 1", "Essa a publicação do usuario 1", 1),
+("Publicacao do usuario 2", "Essa a publicação do usuario 2", 2),
+("Publicacao do usuario 3", "Essa a publicação do usuario 3", 3),
+("Publicacao do usuario 4", "Essa a publicação do usuario 4", 4),
+("Publicacao do usuario 5", "Essa a publicação do usuario 5", 5),
+("Publicacao do usuario 6", "Essa a publicação do usuario 6", 6),
+("Publicacao do usuario 7", "Essa a publicação do usuario 7", 7),
+("Publicacao do usuario 8", "Essa a publicação do usuario 8", 8),
+("Publicacao do usuario 9", "Essa a publicação do usuario 9", 9),
+("Publicacao do usuario 10", "Essa a publicação do usuario 10", 10);
